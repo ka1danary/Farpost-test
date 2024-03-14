688afc34c2e06d6a6c4741ac48f12676952550a2
Potega Xenia <goldenksenia145@gmail.com>
2024-03-14 10:29:50 +1000

    first assemble

---
+++ src/view/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export const App: React.FC = () => {
+    
+    return (
+       <article className={styles.article}>
+            App
+       </article>
+    ) 
+}
